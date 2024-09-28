9853a29006c93b6f0ab18587a6f2d2370e8a3993
akadusei <attakusiadusei@gmail.com>
2024-09-28 20:43:12 +0000

    Make paths consistent

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,9 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('authors/', views.AuthorListView.as_view(), name='authors'),
-    path('authors/<int:pk>/', views.AuthorDetailView.as_view(), name='author-detail'),
+    path('author/<int:pk>/', views.AuthorDetailView.as_view(), name='author-detail'),
     path('books/', views.BookListView.as_view(), name='books'),
-    path('books/<int:pk>/', views.BookDetailView.as_view(), name='book-detail'),
+    path('book/<int:pk>/', views.BookDetailView.as_view(), name='book-detail'),
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
     path('borrowed/', views.BorrowedBooksListView.as_view(), name='all-borrowed'),
     path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
